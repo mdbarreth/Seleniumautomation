8bbc96447b2331cb6aa1ccf49923ec4245df8424
mdbarreth <mohamedbarrethjmc@gmail.com>
2022-04-13 12:51:35 +0530

    American guy

--- src/test/java/stepDefinations/stepDefinitions.java
+++ src/test/java/stepDefinations/stepDefinitions.java
@@ -15,11 +15,13 @@ public class stepDefinitions {
     public void user_is_on_netbanking_landing_page() throws Throwable {
     	System.out.println("Navigated to login url");
     	System.out.println("Git things");
+    	
     }
 
     @When("^User login into application with username and password$")
     public void user_login_into_application_with_username_and_password() throws Throwable {
     	System.out.println("Logged in success");
+    	System.out.println("Git things");
     }
 
     @Then("^Homepage is populated$")
@@ -30,11 +32,13 @@ public class stepDefinitions {
     public void user_login_into_application_with_something_and_password_something(String strArg1, String strArg2) throws Throwable {
     	System.out.println(strArg1);
     	System.out.println(strArg2);
+    	System.out.println("Git things");
     }
 
     @And("^Cards displayed are \"([^\"]*)\"$")
     public void cards_displayed_are_something(String strArg1) throws Throwable {
     	System.out.println(strArg1);
+    	System.out.println("Git things");
     }
     
     }
